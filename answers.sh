fa4bc79420a932e10bce0f25689668455627c5ef
Elina <elliekarout@gmail.com>
2024-12-14 01:23:54 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -26,3 +26,26 @@ WHERE name = "Basma";
 UPDATE students
 SET Points = Points - 30 
 WHERE name = "Alex"; 
+
+
+Create a table
+CREATE TABLE "graduates" (
+	"ID" INTEGER NOT NULL Primary key AUTOINCREMENT,
+	"Name" TEXT NOT NULL UNIQUE,
+	"Age" INTEGER,
+	"Gender" TEXT,
+	"Points" INTEGER,
+	"Graduation" date);
+
+10
+INSERT INTO graduates (name, Age, Gender, Points)
+SELECT name, Age, Gender, Points from students
+WHERE name = "Layal";
+
+11
+UPDATE graduates
+SET Graduation = "2018-09-08"
+WHERE name = "Layal";
+
+12
+DELETE FROM students WHERE name = "Layal";
